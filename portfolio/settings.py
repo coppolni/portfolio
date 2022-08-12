54677466e4e1d125f01791c2be88f48d53210ac5
coppolni <nick.coppola2010@gmail.com>
2022-08-12 15:48:36 -0400

    Adjusted settings and added .gitignore

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,13 +25,13 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 # with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
 #     SECRET_KEY = f.read().strip()
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG')
 
 
 ALLOWED_HOSTS = ['https://nickcoppola.herokuapp.com/', '127.0.0.1:8000']
